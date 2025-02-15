1e296f6d083a0efa9bfd62c82097b6ec9b05e19d
Sawera Tariq <saweratariq567@gmail.com>
2025-02-15 17:46:12 +0500

    navbar

--- src/components/navbar/NavLinks.js
+++ src/components/navbar/NavLinks.js
@@ -7,8 +7,8 @@ import {BsGithub} from "react-icons/bs"
 const NavLinks = ({handleNav}) => {
   return (
     <ul className='nav-links'>
-        <li onClick={handleNav} ><Link  to="//twitter.com/_pappy_jay_" target='_blank' className='nav-link'><FaTwitter /></Link></li>
-        <li onClick={handleNav} ><Link  to="//www.linkedin.com/in/pappyjay" target='_blank' className='nav-link'><FaLinkedin /></Link></li>
+        <li onClick={handleNav} ><Link  to="/" target='_blank' className='nav-link'><FaTwitter /></Link></li>
+        <li onClick={handleNav} ><Link  to="https://www.linkedin.com/in/haya-khan-8a51072aa/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3B%2BQcfFL4bStS9Q9SIIc6c9Q%3D%3D" target='_blank' className='nav-link'><FaLinkedin /></Link></li>
         <li onClick={handleNav} ><Link  to="https://github.com/Saweratariq07/" target='_blank' className='nav-link'><BsGithub /></Link></li>
         <li onClick={handleNav} ><a  href="mailto:saweratariq567@gmail.com" target='_blank' className='nav-link' rel="noreferrer"><BiEnvelope /></a></li>
   </ul>
