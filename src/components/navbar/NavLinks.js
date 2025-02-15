4d9a033c457e5a34d537b9ed04a21f817701e314
Sawera Tariq <saweratariq567@gmail.com>
2025-02-15 18:16:17 +0500

    navbar

--- src/components/navbar/NavLinks.js
+++ src/components/navbar/NavLinks.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaLinkedin, FaTwitter} from "react-icons/fa"
+import { FaLinkedin, FaInstagram} from "react-icons/fa"
 import {BiEnvelope} from "react-icons/bi"
 import {BsGithub} from "react-icons/bs"
 
 const NavLinks = ({handleNav}) => {
   return (
     <ul className='nav-links'>
-        <li onClick={handleNav} ><Link  to="" target='_blank' className='nav-link'><FaTwitter /></Link></li>
-        <li onClick={handleNav} ><Link  to="https://www.linkedin.com/in/sawera-tariq-8a51072aa/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3B8bqxklLiSgOPXmuTr91G3A%3D%3D" target='_blank' className='nav-link'><FaLinkedin /></Link></li>
-        <li onClick={handleNav} ><Link  to="https://github.com/Saweratariq07/" target='_blank' className='nav-link'><BsGithub /></Link></li>
-        <li onClick={handleNav} ><a  href="mailto:saweratariq567@gmail.com" target='_blank' className='nav-link' rel="noreferrer"><BiEnvelope /></a></li>
+<li onClick={handleNav}>
+<a href="https://www.instagram.com/saweratariq567/" target="_blank" rel="noopener noreferrer" className="nav-link">
+    <FaInstagram />
+</a>
+
+</li>
+        <li onClick={handleNav}>
+  <a href="https://www.linkedin.com/in/saweratariq" target="_blank" rel="noopener noreferrer" className="nav-link">
+    <FaLinkedin />
+  </a>
+</li><li onClick={handleNav}>
+  <a href="https://github.com/Saweratariq07/" target="_blank" rel="noopener noreferrer" className="nav-link">
+    <BsGithub />
+  </a>
+</li>
+
+<li onClick={handleNav}>
+  <a href="mailto:saweratariq567@gmail.com" target="_blank" rel="noopener noreferrer" className="nav-link">
+    <BiEnvelope />
+  </a>
+</li>
+
   </ul>
   )
 };
